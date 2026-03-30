b76c6189be75b6575d56e5882cd3686363caf102
DELL <DELL@DESKTOP-7PGB5UI>
2026-03-30 16:18:53 +0530

    Add project files.

---
+++ LoginForm/Login.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="LoginForm.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+        <h2>Insert User</h2>
+
+        <asp:Label ID="lblmsg" runat="server"></asp:Label><br /><br />
+
+        Username:
+        <asp:TextBox ID="txtusername" runat="server"></asp:TextBox><br /><br />
+
+        Password:
+        <asp:TextBox ID="txtpassword" runat="server"></asp:TextBox><br /><br />
+
+        <asp:Button ID="btninsert" runat="server" Text="Insert" OnClick="btninsert_Click" />
+
+        <hr />
+
+        <h2>User List</h2>
+
+        <asp:GridView ID="GridView1" runat="server" AutoGenerateColumns="true"></asp:GridView>
+
+    </form>
+</body>
+</html>
---
+++ LoginForm/Login.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace LoginForm
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=DESKTOP-7PGB5UI;Initial Catalog=normsoft;Integrated Security=True");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                BindGrid(); 
+            }
+        }
+
+    
+        protected void btninsert_Click(object sender, EventArgs e)
+        {
+            con.Open();
+
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO Login (UserName, Password) VALUES (@username, @password)", con);
+
+            cmd.Parameters.AddWithValue("@username", txtusername.Text);
+            cmd.Parameters.AddWithValue("@password", txtpassword.Text);
+
+            int i = cmd.ExecuteNonQuery();
+
+            if (i > 0)
+            {
+                lblmsg.Text = "Inserted Successfully";
+               
+                BindGrid(); 
+            }
+            else
+            {
+                lblmsg.Text = "Insert Failed";
+          
+            }
+
+            con.Close();
+
+            txtusername.Text = "";
+            txtpassword.Text = "";
+        }
+
+        public void BindGrid()
+        {        
+
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM Login", con);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+           
+        }
+    }
+}
